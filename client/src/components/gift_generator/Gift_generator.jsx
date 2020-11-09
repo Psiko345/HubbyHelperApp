fac223a77788f05a2c8b9e409da1ed77f046a765
Joshua Piper <joshua.g.piper@gmail.com>
2020-11-09 15:11:51 +1100

    gift icons should now appear when a gift is generated.

--- client/src/components/gift_generator/Gift_generator.jsx
+++ client/src/components/gift_generator/Gift_generator.jsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import GiftCategories from '../criteria_selectors/GiftSelection'
-import BudgetSelection from '../criteria_selectors/BudgetSelection'
 import { Gifts } from '../criteria_selectors/gifts_list'
 import { Button } from '../Button'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import GiftIcon from './gift-icon-52.png';
+import IconGenerator from './IconGenerator';
 
 
 const useStyles = makeStyles({
@@ -55,7 +51,8 @@ function GiftGenerator({ budget, incomingCategories }) {
             return "$1000+";
         return "Other";
     }
-    // const something = JSON.stringify(budget);
+
+
     let possibleMatchingGifts = Gifts.filter(aGift => isInGiftCategory(aGift, incomingCategories));
     console.log({ possibleMatchingGifts })
 
@@ -81,17 +78,14 @@ function GiftGenerator({ budget, incomingCategories }) {
                         <Typography gutterBottom variant="h5" component="h2">
                             Let's get her some:
                     </Typography>
-                        <CardMedia
-                            className={classes.media}
-                            image={GiftIcon}
-                            title="placeholder gift icon"
-                        />
+                        <IconGenerator chosenGift={chosenGift} />
                         {!chosenGift && <p>No matching gift</p>}
-                        {chosenGift && (<CardContent>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                {chosenGift.name}, with a budget of: {toPriceRange(budget)}
-                            </Typography>
-                        </CardContent>)}
+                        {chosenGift && (
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {chosenGift.name}, with a budget of: {toPriceRange(budget)}
+                                </Typography>
+                            </CardContent>)}
                     </CardActionArea>
                 </Card>
             )}
@@ -99,7 +93,7 @@ function GiftGenerator({ budget, incomingCategories }) {
     )
 }
 
-export default GiftGenerator
+export default GiftGenerator;
 
 // let possibleMatchingGifts = gifts
 // .filter(x => isInPriceRange(x, budget))
--- client/src/components/gift_generator/IconGenerator.js
+++ client/src/components/gift_generator/IconGenerator.js
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from 'react'
-import { chosenGift } from './Gift_generator'
-import necklaceIcon from '../../icons/necklace'
+// import { chosenGift } from './Gift_generator'
+import necklace from './icons/necklace.png';
+import earrings from './icons/earrings.png';
+import ring from './icons/ring.png'
+import { makeStyles } from '@material-ui/core/styles';
+import CardMedia from '@material-ui/core/CardMedia';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        maxHeight: 140,
+    },
+});
+
 
 
 function IconGenerator({ chosenGift }) {
+    const classes = useStyles();
 
-    let chosenGiftIcon = ''
-
-    const matchGiftNameToIcon = () => {
+    const matchGiftNameToIcon = (chosenGift) => {
         if (chosenGift.name === "Necklace")
-            return '../'
+            return necklace
+        if (chosenGift.name === "Earrings")
+            return earrings
+        if (chosenGift.name === "Ring")
+            return ring
+        return "Unknown";
     }
 
+    return (
+        <div>
+            <CardMedia
+                component="img"
+                className={classes.media}
+                src={matchGiftNameToIcon(chosenGift)}
+                alt="gift icon image"
+            />
+        </div>
+    )
 }
+
+export default IconGenerator;
